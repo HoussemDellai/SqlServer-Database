e8e6397727a8c840092c5eb3cb97d7dd962a574d
Emilian BALANESCU <emilian.balanescu@gmail.com>
2019-07-26 14:28:45 +0300

    removed color column.

--- SqlServerDatabase/Product.sql
+++ SqlServerDatabase/Product.sql
@@ -1,7 +1,6 @@
 ﻿CREATE TABLE [dbo].[Product]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+    [Id] INT NOT NULL PRIMARY KEY, 
     [Name] VARCHAR(50) NULL, 
-    [Cost] INT NULL, 
-    [Color] NVARCHAR(50) NULL
+    [Cost] INT NULL
 )
