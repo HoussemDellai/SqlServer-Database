e1a390b32fa411136cb76848d8aedfde73e6805d
subodhde <58364069+subodhde@users.noreply.github.com>
2020-05-11 17:28:14 +0530

    Recreated Product Table

--- SqlServerDatabase/Product.sql
+++ SqlServerDatabase/Product.sql
@@ -1,7 +1,13 @@
-﻿CREATE TABLE [dbo].[Product]
+﻿user [Products]
+
+DROP TABLE [dbo].[Product]
+
+
+CREATE TABLE [dbo].[Product]
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
     [Name] VARCHAR(50) NULL, 
     [Cost] INT NULL, 
-    [Color] NVARCHAR(50) NULL
+    [Color] NVARCHAR(50) NULL,
+    [Size] VARCHAR(50) NOT NULL
 )
